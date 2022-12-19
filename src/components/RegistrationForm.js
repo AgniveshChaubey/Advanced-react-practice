96086973210ed72cb813d2e73b90651b342b9746
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-12-19 17:45:22 +0530

    form is ready, only last step, i.e., form validation is remaining

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,26 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const RegistrationForm = () => {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState('');
+    const [passwd, setPasswd] = useState({
+        value : '',
+        isTouched :false
+    });
+    const [role, setRole] = useState('role');
+
+    const handleSubmit = (e) => {
+        alert('Accoutn created')
+        e.preventDefault();
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPasswd('');
+    }
+
+    const getIsFormValid = () => {
+        if (!firstName || !lastName || !email || !passwd) {
+            return true;
+        }
+        return false;
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit} >
             <fieldset>
                 <h2>Sign Up</h2>
-                <div>
-                    <label htmlFor="firstName">First name</label> <br />
-                    <input type="text" id='fiestName' />
+                <div className='Field'>
+                    <label htmlFor="firstName">First name<sup style={{color :'red'}}>*</sup> </label> <br />
+                    <input type="text"
+                        id='fiestName'
+                        value={firstName}
+                        placeholder='Fiest name'
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
                 </div>
-                <div>
+                <div className='Field'>
                     <label htmlFor="lastName">Last name</label> <br />
-                    <input type="text" id='lastName' />
+                    <input type="text"
+                        id='lastName'
+                        value={lastName}
+                        placeholder='Last name'
+                        onChange={e => setLastName(e.target.value)}
+                    />
                 </div>
-                <div>
-                    <label htmlFor="email">Email address</label> <br />
-                    <input type="text" id='email' />
+                <div className='Field'>
+                    <label htmlFor="email">Email address<sup style={{color :'red'}}>*</sup> </label> <br />
+                    <input type="email"
+                        id='email'
+                        value={email}
+                        placeholder='Email'
+                        onChange={e => setEmail(e.target.value)}
+                    />
                 </div>
-                <div>
-                    <label htmlFor="passwd">Password</label> <br />
-                    <input type="text" id='passwd' />
+                <div className='Field'>
+                    <label htmlFor="passwd">Password<sup style={{color :'red'}}>*</sup></label> <br />
+                    <input type="password"
+                        id='passwd'
+                        value={passwd}
+                        placeholder='Password'
+                        onChange={e => setPasswd(e.target.value)}
+                    />
                 </div>
+                <div className="Field">
+                    <label htmlFor="role">Role<sup style={{color :'red'}}>*</sup></label> <br />
+                    <select id="role" value={role} onChange={e => setRole(e.target.value)}>
+                        <option value="role">Role</option>
+                        <option value="individual">Individual</option>
+                        <option value="business">Business</option>
+                    </select>
+                </div>
+                <button type='submit' disabled={getIsFormValid} >Create Account</button>
             </fieldset>
         </form>
     )
